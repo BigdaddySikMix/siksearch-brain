4d03968cc762669b255caba5e22188a06cee3198
BigdaddySikMix <ch4co2@gmail.com>
2026-05-25 17:36:06 -0700

    Update scan.js

--- netlify/functions/scan.js
+++ netlify/functions/scan.js
@@ -1,6 +1,18 @@
 exports.handler = async function(event) {
+
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 204, headers: corsHeaders, body: '' };
+  }
+
   if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
+    return { statusCode: 405, headers: corsHeaders, body: JSON.stringify({ error: 'Method Not Allowed' }) };
   }
 
   try {
@@ -36,17 +48,9 @@ exports.handler = async function(event) {
     const text = data.content[0].text.trim();
     const cds = JSON.parse(text);
 
-    return {
-      statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ cds })
-    };
+    return { statusCode: 200, headers: corsHeaders, body: JSON.stringify({ cds }) };
 
   } catch(err) {
-    return {
-      statusCode: 500,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ error: err.message })
-    };
+    return { statusCode: 500, headers: corsHeaders, body: JSON.stringify({ error: err.message }) };
   }
 };
